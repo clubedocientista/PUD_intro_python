3bd946d50e398eb7429118d091a3b0f19a3c3adf
Clube do Cientista <clubedocientista@gmail.com>
2020-08-03 15:49:15 -0300

    Add files via upload

--- dia1.py
+++ dia1.py
@@ -18,22 +18,22 @@ Munk2020
 ## Hello World
 print ("Hello World")                          
 # imprime (Out) algo no console
-print ('Bem vindos ao seminário de curso de introdução\nao python do Clube do Cientista')
+print ('Bem vindos ao seminário de introdução ao python')
 # \n = proxima linha ('Enter')
 # \t = tabulação ('Tab')      
 
-PATH = 'D:\\Documentos\\Clube do Cientista\\cursos\\Python_PUD' # muda com SO ('\', '\\', ou '/')
 import os
-os.getcwd()                                      # retorna o diretório atual
-os.chdir(PATH)                                   # altera o diretório para PATH
+os.getcwd()         # retorna o diretório atual
+os.chdir('D:\\Documentos\\Clube do Cientista\\cursos\\Python_PUD')      # altera o diretório para PATH
+# muda com SO ('\', '\\', ou '/')
 os.listdir()            
 
 ## Manuseando diretórios e arquivos
 # explorador de arquivos x explorador de variáveis
 
-arquivos = listdir()                            # lista de arquivos salvo como variável
+arquivos = os.listdir()   # salva lista de arquivos em variável
 import glob
-glob.glob('*.py')                               # arquivos com extensão .py
+glob.glob('*.py')         # retorna arquivos com extensão .py
 glob.glob('*.txt')
 
 print (arquivos)
@@ -43,25 +43,27 @@ arquivos[0]                                     # diferença entre () e []
 dir = 'testes'
 os.mkdir(dir)                                   # criando diretório '\testes'
 os.chdir(dir)                                   # alterando diretório para '\testes'
+os.listdir()
 
-texto1 = 'Hello World\nturma Cousteau2020'
+# salvando arquivo de texto
+texto1 = 'Seminário PUD + Clube do Cientista\nHello World'
 print (texto1)
-
 texto1
 
-texto2 = input ('Qual é o seu nome?\n')         # gravao o input In em variável
+texto2 = input ('Qual é o seu nome?\n')  # salva input em variávell
 print (texto2)
+texto3 = input ('Qual é a sua idade?\n') 
 
-
-file = open('aulas_teste.txt', 'w')             # cria arquivo no modo 'write'
-file.write(texto1+'\n')                         # escreve texto1 e próxima linha
-file.write(texto2)                              # escreve texto2
-file.close()                                    # fecha o arquivo
+file = open('aulas_teste.txt', 'w') # cria arquivo no modo 'write'
+file.write(texto1+'\n')             # escreve texto1 e próxima linha
+file.write(texto2+'\n')          
+file.write(texto3)                      
+file.close()                        # fecha o arquivo
 
 
 # outro modo - identação
 with open('aulas_teste.txt', 'w') as file:
-    file.write(texto1)                          # identação indica as linhas que estão dentro da função 'open'
+    file.write(texto1)             # identação indica as linhas que estão dentro da função 'open'
     file.write('\n\n')
     file.write(texto2)
 
@@ -72,29 +74,17 @@ os.remove('aulas_teste.txt')                    # apagando arquivo
 cd ..
 rmtree ('testes')                               # apagando diretório
 
-
-#  cria diretório se inexistente
-try:                                            # tentativa de executar comandos
-    os.mkdir(dir)
-    print("O diretório " , dir ,  " foi criado ")    
-except FileExistsError:                         # se o erro for do tipo 'FileExistsError
-    print("O diretório " , dir ,  " já existe")
-
-
-rmtree ('testes')
-
-
-
-PATH = ('D:\\Documentos\\Clube do Cientista\\curso Python\\Cousteau2020') # muda com SO ('\', '\\', ou '/')
+## copiar e colar em um novo arquivo e rodar com %runfile
+'''
+PATH = 'D:\\Documentos\\Clube do Cientista\\cursos\\Python_PUD'
 import os
-os.getcwd()                                      # retorna o diretório atual
+os.getcwd()          # retorna o diretório atual
 os.chdir(PATH)   
-name_of_file = input("What is the name of the file: ")
-completeName = os.path.join(name_of_file+".txt")         
-file1        = open(completeName, "w")
-toFile       = input("Write what you want into the field")
-file1.write(toFile)
-file1.close()
-
-print ('O nome do meu arquivo é : ', completeName, '\nO conteudo dele e: ', toFile)
-
+nome_arq = input("Qual será o nome do arquivo: ")
+nome_completo = os.path.join(nome_arq+".txt")         
+arq           = open(nome_completo, "w")
+texto         = input("Escreva o que você quer no arquivo:\n")
+arq.write(texto)
+arq.close()
+print ('O nome do meu arquivo é : ', nome_completo, '\nO conteudo dele é: ', texto)
+'''
